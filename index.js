f08a3eb52aa24acc527246c1f3eb4b3bdcab6eb5
zakintaliban <nukmans23@gmail.com>
2019-11-04 11:42:21 +0700

    project-javascript-data-types

---
+++ index.js
@@ -0,0 +1,82 @@
+console.log("halo dunia");
+console.warn("halo kewaspadaan");
+console.error("halo salah");
+console.info("halo pemberitahuan");
+
+alert("waduh");
+
+// STRING
+console.log("halo dunia");
+
+const name = "Khasbullah Nukman Zakin";
+
+console.log(`Halo ${name}`);
+
+alert(`Halo ${name}`);
+
+// NUMBER
+console.log(0);
+console.log(100);
+console.log(3.14);
+
+const age = 18;
+
+console.log(`Kamu berumur ${age}`);
+
+alert(`kamu berumur ${age}`);
+
+// OBJECT
+console.log({
+  name: "Khasbullah Nukman Zakin",
+  age: 18,
+  superPower: "Main game"
+});
+console.log({
+  name: "Khasbullah Nukman Zakin",
+  age: 18,
+  superPower: "Main game"
+});
+
+const hero = {
+  name: "Khasbullah Nukman Zakin",
+  age: 18,
+  superPower: "Main game"
+};
+
+console.log(hero);
+console.log(hero["name"]);
+console.log(hero.age);
+console.log(hero.superPower);
+
+alert(hero);
+alert(hero["name"]);
+alert(hero.age);
+alert(hero.superPower);
+
+// ARRAY
+console.log([1, 2, 3]);
+console.log([0, 1, 2, 3]);
+console.log(["Khasbullah Nukman Zakin", 18, "your data is mine"]);
+
+const person = ["Khasbullah Nukman Zakin", 18, "your data is mine"];
+
+console.log(person);
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+
+alert(person);
+alert(person[0]);
+alert(person[1]);
+alert(person[2]);
+
+// BOOLEAN
+console.log(true);
+console.log(false);
+console.log(true, false);
+
+const toggle = true;
+
+console.log(`Your data is ${toggle}`);
+
+alert(`Your data is ${toggle}`);
