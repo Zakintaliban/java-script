d4d1b7c038134defadce4fabf6598532186ebfde
zakintaliban <nukmans23@gmail.com>
2019-11-06 13:56:50 +0700

    v5

---
+++ loops.js
@@ -0,0 +1,24 @@
+let iteration = 1
+
+do {
+  console.log(iteration)
+  iteration += 5
+} while (iteration < 100)
+
+// Feel free to have some experiments with comparisons
+
+for (let index = 0; index <= 100; index += 10) {
+    console.log(index)
+  }
+  
+  // Feel free to have some experiments with comparisons
+
+  const numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+
+  for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index]
+    console.log(element)
+  }
+  
+  // Feel free to have some experiments with comparisons
+  
